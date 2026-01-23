f19f5d9396e642042393875a5f6ab59aaf5ce293
peng <robertjustinianpeng@gmail.com>
2026-01-22 22:10:46 -0700

    serverless only for ai parse doc

---
+++ document_ingest_poc/src/document_ingest_poc_etl/transformations/document_content_transform.py
@@ -0,0 +1,16 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import col, expr
+
+
+@dp.table(name="documents_content_transform", comment="Binary documents with metadata")
+def documents_content_transform():
+    return (
+        spark.readStream.format("delta")
+        .table("dev_appian_poc.00_bronze.appian_raw_documents_ingest")
+        .withColumn("parsed_document", expr("ai_parse_document(content)"))
+        .select(
+            col("parsed_document"),
+            col("path"),
+            col("modificationTime").alias("updatedTime"),
+        )
+    )
---
+++ document_ingest_poc/src/document_ingest_poc_etl/transformations/raw_document_ingest.py
@@ -0,0 +1,12 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import col
+
+
+@dp.table(name="appian_raw_documents_ingest", comment="Binary documents with metadata")
+def appian_raw_document_ingest():
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "binaryFile")
+        .option("pathGlobFilter", "*{pdf, docx, xlsx, png, jpg}")
+        .load("/Volumes/dev_appian_poc/00_bronze/landing_zone/file_uploads/")
+    )
